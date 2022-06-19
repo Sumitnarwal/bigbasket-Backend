7ba53cd484d3c850cfcbfdf79d86ec8b8acbb8c5
Sumitnarwal <sumitnarwal39@gmail.com>
2022-06-19 21:11:40 +0530

    last

--- src/controller/adToCartController.js
+++ src/controller/adToCartController.js
@@ -1,41 +1,37 @@
 
-// const express = require("express");
-// const User = require("../models/addtoCart");
-// const router = express.Router();
+const express = require("express");
+const User = require("../models/addtoCart");
+const router = express.Router();
 
 
 
-// router.get("", async (req, res) => {
-//     // try {
-//     //   const page=req.query.page
-//     //   const pagesize=req.query.size || 55
-//     //   const skip=(page-1)*pagesize
+router.get("", async (req, res) => {
+  try{
+  const user = await User.find().lean().exec();
+  return res.send(user);
+} catch (err) {
+  return res.status(500).send({ message: err.message })
+}
+});
 
-//       const user = await User.find().lean().exec();
-//         return res.send(user);
-//     } catch (err) {
-//         return res.status(500).send({ message: err.message })
-//     }
-// });
+router.post("", async (req, res) => {
 
-// router.post("", async (req, res) => {
-    
-//     try {
-//         const users = await User.create(req.body);
-//         return res.send(users);
-//     } catch (err) {
-//         return res.status(500).send({ message: err.message })
-//     }
-// });
+  try {
+    const users = await User.create(req.body);
+    return res.send(users);
+  } catch (err) {
+    return res.status(500).send({ message: err.message })
+  }
+});
 
-// router.delete("/:id", async (req, res) => {
-//     try {
-//       const user = await User.findByIdAndDelete(req.params.id).lean().exec();
-  
-//       res.send(user);
-//     } catch (err) {
-//       return res.status(500).send(err.message);
-//     }
-//   }); 
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id).lean().exec();
 
-// module.exports = router;
+    res.send(user);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -8,17 +8,17 @@ app.use(express.json())
 app.use(cors());
 
 const userController = require("./controller/user.controller")
-//const addtoCartController = require("./controller/adToCartController")
+const addtoCartController = require("./controller/adToCartController")
 const vegitableController = require("./controller/veg.Contr")
-//const userControllerrr = require("./controller/userrr.controller")
-//const vegetableCartController = require("./controller/vegetablecart.Controller")
+const userControllerrr = require("./controller/userrr.controller")
+const vegetableCartController = require("./controller/vegetablecart.Controller")
 
-//app.use("/collection", userControllerrr)
-//app.use("/vegetablecart",vegetableCartController)
+app.use("/collection", userControllerrr)
+app.use("/vegetablecart",vegetableCartController)
 
 app.use("/softdrink", userController)
 app.use("/veg", vegitableController)
-//app.use("/addtocart", addtoCartController)
+app.use("/addtocart", addtoCartController)
  
 
 
